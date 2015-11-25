f21e7eaf6ce2ad33bd882fda1b50da6307097985
Louis Meckes <louis.meckes@gmail.com>
2015-11-25 15:37:11 +0100

    graph traversal

--- src/test/scala/Neo4jSpec.scala
+++ src/test/scala/Neo4jSpec.scala
@@ -1,10 +1,14 @@
 import java.io.File
 import java.util
 
+import org.neo4j.cypher.internal.compiler.v2_2.functions.Rels
 import org.neo4j.graphdb.index.UniqueFactory
 import org.neo4j.graphdb._
 import org.neo4j.graphdb.factory.GraphDatabaseFactory
+import org.neo4j.graphdb.traversal.Uniqueness
 import org.scalatest._
+import scala.collection.JavaConversions._
+import scala.util.Random
 
 object DbServer {
   def runDb: GraphDatabaseService = {
@@ -71,17 +75,17 @@ class Neo4jSpec extends FlatSpec with Matchers {
       implicit def conv(rt: RelTypes) = new RelationshipType() {def name = rt.toString}
     }
 
-    val text = """ Le petit juge blond est ivre """
+    val text = """ Le petit juge blond est ivre """.toLowerCase
 
     var tx: Transaction = null
     try {
       tx = grapdDb.beginTx
 
       // Get or Create unique Char Nodes
-      val uniqueCharNodeFactory = new UniqueFactory.UniqueNodeFactory( grapdDb, "chars" ) {
+      val uniqueCharNodeFactory = new UniqueFactory.UniqueNodeFactory(grapdDb, "chars") {
         override def initialize(n: Node, prop: util.Map[String, AnyRef]): Unit = {
-          n.addLabel( DynamicLabel.label( "Char" ) );
-          n.setProperty( "char", prop.get( "char" ) );
+          n.addLabel(DynamicLabel.label("Char"));
+          n.setProperty("char", prop.get("char"));
         }
       };
 
@@ -89,16 +93,38 @@ class Neo4jSpec extends FlatSpec with Matchers {
       text.zipWithIndex.foreach(
         c => {
           val ucn = uniqueCharNodeFactory.getOrCreate("char", c._1)
-          val next = c._2+1
+          val next = c._2 + 1
           if (text.length > next)
             ucn.createRelationshipTo(uniqueCharNodeFactory.getOrCreate("char", text(next)), RelTypes.NEIGHBOR)
         }
       )
 
+      // Traverse Graph
+      val neighborTraversal = grapdDb.traversalDescription()
+        .depthFirst()
+        .relationships(RelTypes.NEIGHBOR)
+        .uniqueness(Uniqueness.NODE_GLOBAL);
+      val randomStartNode = uniqueCharNodeFactory.getOrCreate("char", text(Random.nextInt(text.length)))
+      neighborTraversal.traverse(randomStartNode).nodes().toList.foreach(
+        n => {
+          val char = n.getProperty("char")
+          val degree = n.getDegree.toString
+          println(s"$char: degree=$degree")
+        }
+      )
+
       tx.success()
+
     } finally {
       tx.close()
     }
+
+    /*for ( Node currentNode : friendsTraversal
+      .traverse( node )
+      .nodes() )
+    {
+      output += currentNode.getProperty( "name" ) + "\n";
+    }*/
 
     grapdDb.shutdown()
   }
